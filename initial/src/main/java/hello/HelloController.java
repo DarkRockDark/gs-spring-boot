06397b9a23a7ef394e1d2134ac7cba0763cb961e
Douglas Rogers <edwardDouglasRogers@gmail.com>
2018-04-03 22:08:19 -0400

    RestController vs Controller

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -1,14 +1,19 @@
 package hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping({"/restcontrol", "/restControl", "/restController"})
+    public String index(@RequestParam(name="name", required=false, defaultValue = "World")String name, Model model) {
+        model.addAttribute("name", name);
+        System.out.println("in rest Controller");
+        return "restpage - not serving HTML";
     }
     
 }
